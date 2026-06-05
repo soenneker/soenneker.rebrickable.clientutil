f091637f3b335e0ff51cc73451aec497fa29c254
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:39 -0500

    xml commenting

--- src/Soenneker.Rebrickable.ClientUtil/Abstract/IRebrickableClientUtil.cs
+++ src/Soenneker.Rebrickable.ClientUtil/Abstract/IRebrickableClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Rebrickable.ClientUtil.Abstract;
 /// </summary>
 public interface IRebrickableClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<RebrickableOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
